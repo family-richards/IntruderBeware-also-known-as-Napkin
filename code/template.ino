2d07ce590dd58f4aca0371f1b0138863b5fa8571
kendell <kendell.r@outlook.com>
2019-04-14 08:32:50 -0700

    Update template.ino

--- code/template.ino
+++ code/template.ino
@@ -1,10 +1,11 @@
 void setup() {
   pinMode(32, OUTPUT); // set siren pin to output
+  // setup I/O config for smoke detection here
 }
 
 void loop() {
   if (isThereSmoke()) {
-    
+    activateSiren();
   }
 }
 
@@ -15,5 +16,12 @@ bool isThereSmoke() {
 }
 
 void activateSiren() {
-  
+  for (int i = 1000; i < 2000; i += 10) {
+    tone(32, i, 20);
+    delay(20);
+  }
+  for (int i = 2000; i > 1000; i -= 10) {
+    tone(32, i, 20);
+    delay(20);
+  }
 }
