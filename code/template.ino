4edf5b1004938c622387a9ef7371be42b1cf4dc9
kendell <kendell.r@outlook.com>
2019-04-14 08:11:38 -0700

    Create template.ino

---
+++ code/template.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(32, OUTPUT); // set siren pin to output
+}
+
+void loop() {
+  if (isThereSmoke()) {
+    
+  }
+}
+
+bool isThereSmoke() {
+  // Place your code to read/write to the IO pins to detect smoke
+  bool smoke = false;
+  return smoke;
+}
+
+void activateSiren() {
+  
+}
