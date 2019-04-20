d46b38526a1b9d192c9a7906fd2020204932b661
kendell <kendell.r@outlook.com>
2019-04-20 06:51:01 -0700

    Update settinggetter.ino

--- code/settinggetter/settinggetter.ino
+++ code/settinggetter/settinggetter.ino
@@ -1,1 +1,39 @@
 #include "EEPROM.h"
+#include <Adafruit_NeoPixel.h>
+#define EEPROM_SIZE 16
+void setup() {
+  while (!Serial);
+  Serial.begin(9600);
+  Serial.println("Welcome to the IntruderBeware setting getter! Give me a second to set up the EEPROM...");
+  if (!EEPROM.begin(EEPROM_SIZE)){
+    Serial.println("Failed to initialise EEPROM!");
+    while(true);
+  }
+  Serial.println("Done! First, I'll find out if your PIR is reverse.");
+  if (EEPROM.read(0) == 0) {
+    Serial.println("It's not reverse!");
+  } else if (EEPROM.read(0) == 1) {
+    Serial.println("It's reverse!");
+  } else {
+    Serial.println("Hmm... I'm having trouble.");
+  }
+  Serial.println("I think you have "+String(EEPROM.read(1))+" NeoPixels.");
+  switch (EEPROM.readUShort(2)) {
+    case NEO_KHZ400+NEO_RGB:
+      Serial.print("I think you have V1 RGB NeoPixels.");
+      break;
+    case NEO_KHZ800+NEO_GRB:
+      Serial.print("I think you have V2 RGB NeoPixels.");
+      break;
+    case NEO_KHZ800+NEO_RGBW:
+      Serial.print("I think you have RGBW NeoPixels.");
+      break;
+    default:
+      Serial.println("Hmm... I'm having trouble.");
+  }
+  Serial.println("All fetchable settings have been fetched.");
+}
+
+void loop() {
+  
+}
