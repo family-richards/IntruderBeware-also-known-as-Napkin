e56712fe7ad22f1e06e4794d9cf08e35d8290af0
kendell <kendell.r@outlook.com>
2019-04-19 18:21:33 -0700

    Update setupwizard.ino

--- code/setupwizard/setupwizard.ino
+++ code/setupwizard/setupwizard.ino
@@ -143,9 +143,9 @@ void loop() {
 }
 
 void colorWipe(uint32_t c, uint16_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
+  for(uint16_t i=0; i<strippy.numPixels(); i++) {
+    strippy.setPixelColor(i, c);
+    strippy.show();
     delay(wait);
   }
 }
